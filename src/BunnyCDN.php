f9c8fde554eb399be8cf2a3bca74036f3b6ddaee
Leon <leon@wp2static.com>
2019-05-20 03:32:43 +0700

    http1.1 on all Bunny methods

--- src/BunnyCDN.php
+++ src/BunnyCDN.php
@@ -114,6 +114,7 @@ class BunnyCDN extends SitePublisher {
             curl_setopt( $ch, CURLOPT_URL, $endpoint );
             curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
             curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+            curl_setopt( $ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1 );
             curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
             curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 0 );
             curl_setopt( $ch, CURLOPT_POST, 1 );
@@ -166,6 +167,7 @@ class BunnyCDN extends SitePublisher {
             curl_setopt( $ch, CURLOPT_URL, $remote_path );
             curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
             curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+            curl_setopt( $ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1 );
             curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
             curl_setopt( $ch, CURLOPT_HEADER, 0 );
             curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
